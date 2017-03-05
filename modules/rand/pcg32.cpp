9d0bbbc0879e62d73ffe5bd8036323c1391faa84
Leandro Motta Barros <lmb@stackedboxes.org>
2017-03-05 02:00:48 -0300

    Add PCG32

---
+++ modules/rand/pcg32.cpp
@@ -0,0 +1,86 @@
+/*************************************************************************/
+/*  pcg32.cpp                                                            */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                    http://www.godotengine.org                         */
+/*************************************************************************/
+/* Copyright (c) 2017 Leandro Motta Barros.                              */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+//
+// The code in this file is derived from the C implementation originally found
+// at https://github.com/imneme/pcg-c-basic/
+//
+// That code is Copyright 2014 Melissa O'Neill <oneill@pcg-random.org> and was
+// originally licensed under the Apache License, Version 2.0
+// (http://www.apache.org/licenses/LICENSE-2.0).
+//
+// For additional information about the PCG random number generation scheme,
+// including its license and other licensing options, visit
+//
+//     http://www.pcg-random.org
+//
+
+#include "pcg32.h"
+#include <limits>
+
+
+RandPCG32::RandPCG32()
+	: state(0x853c49e6748fea9bULL), inc(0xda3e39cb94b95bdbULL) {
+	// Nothing here.
+}
+
+
+RandPCG32::~RandPCG32() {
+	// Nothing here.
+}
+
+
+uint64_t RandPCG32::random() {
+	const uint64_t oldstate = state;
+	state = oldstate * 6364136223846793005ULL + inc;
+	const uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+	const uint32_t rot = oldstate >> 59u;
+	return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
+
+uint64_t RandPCG32::max() {
+	return std::numeric_limits<uint32_t>::max();
+}
+
+void RandPCG32::seed(uint64_t p_seed) {
+	const uint64_t seq = 192837465ULL;
+
+	state = 0U;
+	inc = (seq << 1u) | 1u;
+	random();
+	state += p_seed;
+	random();
+}
+
+void RandPCG32::_bind_methods() {
+	// All exported methods are declared in the superclass.
+}
+
+
+// TODO: Add tests!
---
+++ modules/rand/pcg32.h
@@ -0,0 +1,58 @@
+/*************************************************************************/
+/*  pcg32.h                                                              */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                    http://www.godotengine.org                         */
+/*************************************************************************/
+/* Copyright (c) 2017 Leandro Motta Barros.                              */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#ifndef RAND_PCG32_H
+#define RAND_PCG32_H
+
+#include "rand.h"
+
+class RandPCG32: public Rand {
+	GDCLASS(RandPCG32, Rand);
+
+	// The state itself.
+	uint64_t state;
+
+	// The selected RNG stream; must be and odd number.
+	uint64_t inc;
+
+protected:
+	static void _bind_methods();
+
+	virtual uint64_t random();
+
+	virtual uint64_t max();
+
+	virtual void seed(uint64_t seed);
+
+public:
+	RandPCG32();
+	virtual ~RandPCG32();
+};
+
+#endif // RAND_PCG32_H
